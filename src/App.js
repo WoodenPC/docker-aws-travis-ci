a4443fe4cfdedc8b9224678b7348461dba101cb2
rasul <stiflerovich@mail.ru>
2019-01-04 21:42:23 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Reactqweqwqwe123123
           </a>
         </header>
       </div>
